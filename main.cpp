68c5b06be62b3795e4c91715d4e35169833de43d
WhiteWryrm14 <stefano165@outlook.it>
2025-06-16 17:04:23 +0200

    Add Hello World C++ 26 on CLion 25

--- main.cpp
+++ main.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
-#include "Calculator.h"
-
 using namespace std;
 
-int main()
-{
-    double x = 0.0;
-    double y = 0.0;
-    double result = 0.0;
-    char oper = '+';
-
-    cout << "Calculator Console Application" << endl << endl;
-    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
-
-    Calculator c;
-    while (true)
-    {
-        cin >> x >> oper >> y;
-        if (oper == '/' && y == 0)
-        {
-            cout << "Math error: Attempted to divide by zero!" << endl;
-
-        }
-        else
-        {
-            result = c.Calculate(x, oper, y);
-        }
-        cout << "Result " << "of " << x << oper << y << " is: " << result << endl;
-    }
-
+int main() {
+    cout << "Hello word!" << endl;
     return 0;
 }
